b6af72d7d65a28bbcc35166788ae0695e6103f1b
dcamusa <45017346+dcamusa@users.noreply.github.com>
2019-05-24 10:27:44 -0500

    cambios diseño

---
+++ src/app/Shared/menu-desplegable/menu-desplegable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-desplegable',
+  templateUrl: './menu-desplegable.component.html',
+  styleUrls: ['./menu-desplegable.component.sass']
+})
+export class MenuDesplegableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
