88461b9e4efa74687b11be35d19a6117b0043e23
Piero Bravo <pbravogutarra@outlook.com>
2019-06-10 15:36:31 -0500

    mejora en el diseño del formulario y se agrega adjuntar imagenes

--- src/app/components/procesos/registroNoConformidad/formulario-rnc/formulario-rnc.component.ts
+++ src/app/components/procesos/registroNoConformidad/formulario-rnc/formulario-rnc.component.ts
@@ -100,6 +100,8 @@ export class FormularioRncComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    document.getElementById('files').addEventListener('change', this.onFileSelected, false);
+
     this.rncModel.CodigoProyecto = this._activate_route.snapshot.params[
       "codigoProyecto"
     ];
@@ -130,4 +132,36 @@ export class FormularioRncComponent implements OnInit {
     this.toastr.success('Registro exitoso','Mantenimiento exitoso.');
     this.router.navigate(["/listado-rnc", this.rncModel.CodigoProyecto]);
   }
+
+  onFileSelected(event) {
+    if(event.target.files.length > 0) 
+     {
+       console.log(event.target.files[0].name);
+       var files = event.target.files; 
+
+         for (var i = 0, f; f = files[i]; i++) {
+  
+          // Only process image files.
+          if (!f.type.match('image.*')) {
+            continue;
+          }
+          var reader = new FileReader();
+  
+          // Closure to capture the file information.
+          reader.onload = (function(theFile) {
+            return function(e) {
+              // Render thumbnail.
+              var span = document.createElement('span');
+              span.innerHTML = ['<img style="width: 120px;" class="thumb" src="', e.target.result,
+                                '" title="', escape(theFile.name), '"/>'].join('');
+                                
+              document.getElementById('list').insertBefore(span, null);
+            };
+          })(f);
+    
+          // Read in the image file as a data URL.
+          reader.readAsDataURL(f);
+        }  
+     }
+   }
 }
