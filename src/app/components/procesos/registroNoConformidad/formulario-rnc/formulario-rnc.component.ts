699ec1a96f66922c49ce0a11c2478ed95ae1f784
DBAOscar <adrian_ortiz92>
2019-07-04 08:52:25 -0500

    Logica de fotos al 25%

--- src/app/components/procesos/registroNoConformidad/formulario-rnc/formulario-rnc.component.ts
+++ src/app/components/procesos/registroNoConformidad/formulario-rnc/formulario-rnc.component.ts
@@ -4,18 +4,18 @@ import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { DatePipe } from '@angular/common';
 
-import { Image } from "../../../../interfaces/procesos/registroNoConformidad/registro-no-conformidad";
-import { RegistroNoConformidadService } from "../../../../services/procesos/registroNoConformidad/registro-no-conformidad.service";
-import { RegistroNoConformidad } from "../../../../interfaces/procesos/registroNoConformidad/registro-no-conformidad";
-import { ImagenNoConformidadService } from "../../../../services/procesos/registroNoConformidad/imagen-no-conformidad.service";
+import { Fotos } from '../../../../interfaces/procesos/registroNoConformidad/registro-no-conformidad';
+import { RegistroNoConformidadService } from '../../../../services/procesos/registroNoConformidad/registro-no-conformidad.service';
+import { RegistroNoConformidad } from '../../../../interfaces/procesos/registroNoConformidad/registro-no-conformidad';
+import { ImagenNoConformidadService } from '../../../../services/procesos/registroNoConformidad/imagen-no-conformidad.service';
 
 import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
 import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 
 @Component({
-  selector: "app-formulario-rnc",
-  templateUrl: "./formulario-rnc.component.html",
-  styleUrls: ["./formulario-rnc.component.sass"],
+  selector: 'app-formulario-rnc',
+  templateUrl: './formulario-rnc.component.html',
+  styleUrls: ['./formulario-rnc.component.sass'],
   providers: [
     {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
     {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
@@ -25,82 +25,80 @@ export class FormularioRncComponent implements OnInit {
   rncModel: RegistroNoConformidad = {};
   nuevo: boolean = false;
   ListaArea = [
-    "-- No Describe --",
-    "Almacen",
-    "Calidad",
-    "OT",
-    "Produccion",
-    "Ingenieria",
-    "Procura",
-    "PdR/GA",
-    "Administracion",
-    "Topografia",
-    "Laboratorio"
+    '-- No Describe --',
+    'Almacen',
+    'Calidad',
+    'OT',
+    'Produccion',
+    'Ingenieria',
+    'Procura',
+    'PdR/GA',
+    'Administracion',
+    'Topografia',
+    'Laboratorio'
   ];
   ListaDefecto = [
-    "Abarquillado",
-    "Accesorio Defectuoso",
-    "Acero - defectos",
-    "Acolchonamiento",
-    "Albañilería sub estándar",
-    "Almacenamiento subestándar",
-    "Ausencia de Pase",
-    "Bruñas - Falta/Mal ubicada/Incompleta/Mal acabada",
-    "Cajoneo/Soplado",
-    "Cangrejera"
+    'Abarquillado',
+    'Accesorio Defectuoso',
+    'Acero - defectos',
+    'Acolchonamiento',
+    'Albañilería sub estándar',
+    'Almacenamiento subestándar',
+    'Ausencia de Pase',
+    'Bruñas - Falta/Mal ubicada/Incompleta/Mal acabada',
+    'Cajoneo/Soplado',
+    'Cangrejera'
   ];
-  ListaStatus = ["Abierto", "Anulado", "Cerrado"];
+  ListaStatus = ['Abierto', 'Anulado', 'Cerrado'];
   ListaNombreOriginador = [
-    "Abner Delgado Gomez",
-    "Abrahan Ochante Tineo",
-    "Adan Huaman Canales",
-    "Adbel Sierra Carhuamaca",
-    "Adolfo Cordova Peña",
-    "Adolfo Orrego Chumo",
-    "Adrian Salinas Quispe",
-    "Agustin Yoza Levano",
-    "Aissa Zapata Reyes",
-    "Akira Madueño Reynoso",
-    "Alan Aldave",
-    "Alan Espinoza",
-    "Alberto Bermudez Monteyro"
+    'Abner Delgado Gomez',
+    'Abrahan Ochante Tineo',
+    'Adan Huaman Canales',
+    'Adbel Sierra Carhuamaca',
+    'Adolfo Cordova Peña',
+    'Adolfo Orrego Chumo',
+    'Adrian Salinas Quispe',
+    'Agustin Yoza Levano',
+    'Aissa Zapata Reyes',
+    'Akira Madueño Reynoso',
+    'Alan Aldave',
+    'Alan Espinoza',
+    'Alberto Bermudez Monteyro'
   ];
   ListaTipoReporte = [
-    { codigo: "RNC", descripcion: "No Conformidad" },
-    { codigo: "PNC", descripcion: "Producto No Conforme" },
-    { codigo: "PL", descripcion: "Punch List" },
-    { codigo: "ROB", descripcion: "Reporte de Observaciones" }
+    { codigo: 'RNC', descripcion: 'No Conformidad' },
+    { codigo: 'PNC', descripcion: 'Producto No Conforme' },
+    { codigo: 'PL', descripcion: 'Punch List' },
+    { codigo: 'ROB', descripcion: 'Reporte de Observaciones' }
   ];
   ListaTratamientoNoConformidad = [
-    "-- No Describe --",
-    "Modificar",
-    "Rechazar",
-    "Reclasificar",
-    "Reparar",
-    "Re-trabajo",
-    "Usar como esta"
+    '-- No Describe --',
+    'Modificar',
+    'Rechazar',
+    'Reclasificar',
+    'Reparar',
+    'Re-trabajo',
+    'Usar como está'
   ];
   ListaEjecutorDanio = [
-    "DISTRIBUIDORA PREMIUM S.A.",
-    "EMPAQUETADURAS Y OPERACIONES E.I.R.LTDA.",
-    "PROMETAL ROCA HERMANOS E.I.R.L.",
-    "SOCIEDAD INDUSTRIAL VULCANO SRL",
-    "SUMINISTROS FERMAR S.A.C.",
-    "3D TOPOGRAFIA SOCIEDAD ANONIMA CERRADA - 3D TOPOGRAFIA S.A.C.",
-    "3G OFFICE PERU S.A.C.",
-    "3M PERU SA",
-    "3P & CIA S.A.C",
-    "A & A Edificaciones Generales S.A.",
-    "A B SEGURIDAD E.I.R.L",
-    "A Y A EDIFICACIONES S.A.C",
-    "A.R Acabados Construcción EIRL",
-    "AB7 S.A.C."
+    'DISTRIBUIDORA PREMIUM S.A.',
+    'EMPAQUETADURAS Y OPERACIONES E.I.R.LTDA.',
+    'PROMETAL ROCA HERMANOS E.I.R.L.',
+    'SOCIEDAD INDUSTRIAL VULCANO SRL',
+    'SUMINISTROS FERMAR S.A.C.',
+    '3D TOPOGRAFIA SOCIEDAD ANONIMA CERRADA - 3D TOPOGRAFIA S.A.C.',
+    '3G OFFICE PERU S.A.C.',
+    '3M PERU SA',
+    '3P & CIA S.A.C',
+    'A & A Edificaciones Generales S.A.',
+    'A B SEGURIDAD E.I.R.L',
+    'A Y A EDIFICACIONES S.A.C',
+    'A.R Acabados Construcción EIRL',
+    'AB7 S.A.C.'
   ];
 
-  //listURL   : Array<Image>;
-  listImage: Array<any>;
-
-  //listURL = [Image]
+  // listImage: Array<any>;
+  ListaFotos: Fotos[] = [];
 
   constructor(
     private service: RegistroNoConformidadService,
@@ -124,15 +122,6 @@ export class FormularioRncComponent implements OnInit {
         this.rncModel = result[0].payload.doc.data();
         this.rncModel.Key = result[0].payload.doc._key.path.segments[6];
         this.rncModel.FechaEmision = result[0].payload.doc.data().FechaEmision.toDate();
-
-
-        // const fecha = this.datepipe.transform(result[0].payload.doc.data().FechaEmision.toDate(), 'dd-MM-yyyy');
-        // this.rncModel.FechaEmision = new Date(fecha);
-
-        // console.log(this.rncModel);
-        // console.log(result[0].payload.doc.data());
-        // let lista = this.DescargarImagen(this.rncModel.ListaImagenes);
-        // this.CargarImagen(lista);
       });
     } else {
       this.nuevo = true;
@@ -145,19 +134,25 @@ export class FormularioRncComponent implements OnInit {
 
   Grabar(rncModel) {
     debugger;
-    this.rncModel.FechaEmision=rncModel.FechaEmision._d
+
+    if (this.ListaFotos.length === 0) {
+      this.toastr.warning('Debe tener al menos una foto.', 'Advertencia');
+      return;
+    }
+
+    this.rncModel.FechaEmision = rncModel.FechaEmision._d;
     if (this.nuevo) {
       // Subimos las imagenes
       // this.SubirImagen(this.listImage);
-            //Seteamos número.
-      this.rncModel.Nro = this.rncModel.CodigoProyecto + " - " + this.rncModel.TipoReporte + " - " + this.rncModel.NombreOriginador + " - " + this.rncModel.HHTrabajo;
-      //Seteamos ListaImagenes.
-      //this.rncModel.ListaImagenes = this.listImage;
-      //Llamamos al método guardar.
+      // Seteamos número.
+      this.rncModel.Nro = this.rncModel.CodigoProyecto + ' - ' + this.rncModel.TipoReporte + ' - ' + this.rncModel.NombreOriginador + ' - ' + this.rncModel.HHTrabajo;
+      // Seteamos ListaImagenes.
+      // this.rncModel.ListaImagenes = this.listImage;
+      // Llamamos al método guardar.
       this.service.createRNC(this.rncModel).then(result => {
         debugger;
         console.log(result);
-        //result._key.path.segments[1] aca devuelve el key
+        // result._key.path.segments[1] aca devuelve el key
       });
     } else {
       this.toastr.success('Ha ocurrido un error, comuniquese con el administrador del sistema', 'Advertencia');
@@ -167,39 +162,44 @@ export class FormularioRncComponent implements OnInit {
   SeleccionarImagen(event) {
     if (event.target.files.length > 0) {
       var files = event.target.files;
-      this.listImage = files;
+      //this.listImage = files;
+      let objFotos: Fotos = {};
+      objFotos.ExtensionImagen = files[0].type.split('/')[1];
+      objFotos.NombreImagen = files[0].name.split('.' + objFotos.ExtensionImagen)[0];
 
       for (var i = 0, f; (f = files[i]); i++) {
         // Only process image files.
-        if (!f.type.match("image.*")) {
+        if (!f.type.match('image.*')) {
           continue;
         }
-        var reader = new FileReader();
+        const reader = new FileReader();
 
         // Closure to capture the file information.
         reader.onload = (function(theFile) {
           return function(e) {
             // Render thumbnail.
-            var span = document.createElement("span");
+            const span = document.createElement('span');
+            objFotos.Imagen = e.target.result.split('base64,')[1];
+            
             span.innerHTML = [
               '<img style="width: 120px;" class="thumb" src="',
               e.target.result,
               '" title="',
               escape(theFile.name),
               '"/>'
-            ].join("");
+            ].join('');
 
-            document.getElementById("list").insertBefore(span, null);
+            document.getElementById('list').insertBefore(span, null);
           };
         })(f);
-
+        this.ListaFotos.push(objFotos);
         // Read in the image file as a data URL.
         reader.readAsDataURL(f);
       }
     }
   }
 
-  SubirImagenBytes(listImage){
+  SubirImagenBytes(listImage) {
     this.firebaseStorage.guardarImagenByte(listImage);
   }
 
